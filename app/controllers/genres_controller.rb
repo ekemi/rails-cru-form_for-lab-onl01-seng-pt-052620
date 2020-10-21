4b6cea162eeee1b922cbc534822c49ea7fb6ce47
edmundo papi <edmundopierre@yahoo.com>
2020-10-21 07:40:15 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,32 +1,31 @@
 class GenresController < ApplicationController
+  
   def show
-      @genre = Genre.find(params[:id])
-  end
-
-  def edit
-     @genre = Genre.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def new
-     @genre = Genre.new
+    @genre = Genre.new
   end
 
   def create
-     @genre = Genre.create(params_genre)
-     redirect_to artist_path(@genre)
+    @genre = Genre.create(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
-
     @genre = Genre.find(params[:id])
-    @genre.update(params_genre)
+    @genre.update(genre_params)
     redirect_to genre_path(@genre)
   end
 
   private
-
   def genre_params
-     params.require(:genre).permit(:name)
+    params.require(:genre).permit(:name)
   end
 
 end
