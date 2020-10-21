4ba064a78c3cfae7c1d18cf3b455aaffee7e0c5e
edmundo papi <edmundopierre@yahoo.com>
2020-10-21 07:23:43 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,32 @@
+class GenresController < ApplicationController
+  def show 
+      @genre = Genre.find(params[:id])
+  end
+
+  def edit 
+     @genre = Genre.find(params[:id])
+  end
+
+  def new 
+     @genre = Genre.new
+  end
+
+  def create
+     @genre = Genre.create(params_genre)
+     redirect_to artist_path(@genre)
+  end
+
+  def update 
+
+    @genre = Genre.find(params[:id])
+    @genre.update(params_genre)
+    redirect_to genre_path(@genre)
+  end
+  
+  private
+
+  def genre_params
+     params.require(:genres).permit(:name)
+  end
+
+end
